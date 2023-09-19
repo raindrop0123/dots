f564ba8dbb2588578e791fb648703423799ed67e
raindrop0123 <139353546+raindrop0123@users.noreply.github.com>
2023-09-19 16:08:56 +0800

    Add files via upload

---
+++ awesome/theme.lua
@@ -0,0 +1,143 @@
+---- Load Packages
+local theme_assets = require('beautiful.theme_assets')
+local xresources = require('beautiful.xresources')
+local dpi = xresources.apply_dpi
+local gfs = require('gears.filesystem')
+local themes_path = gfs.get_themes_dir()
+local theme = {}
+
+---- General
+theme.font = 'sans bold 10'
+theme.bg_normal = '#000000'
+theme.bg_focus = '#000000'
+theme.bg_urgent = '#ff0000'
+theme.bg_minimize = '#000000'
+theme.bg_systray = '#000000'
+theme.fg_normal = '#ffffff'
+theme.fg_focus = '#ffffff'
+theme.fg_urgent = '#000000'
+theme.fg_minimize = '#ffffff'
+
+---- Gap
+theme.useless_gap = dpi(3)
+
+---- Border
+theme.border_width = dpi(2)
+theme.border_normal = '#000000'
+theme.border_focus = '#ff62d4'
+theme.border_marked = '#ff0000'
+
+---- Taglist
+theme.taglist_font = 'sans bold 12'
+theme.taglist_bg_focus = '#000000'
+theme.taglist_bg_occupied = '#000000'
+theme.taglist_bg_empty = '#000000'
+theme.taglist_fg_focus = '#ff62d4'
+theme.taglist_fg_occupied = '#ffffff'
+theme.taglist_fg_empty = '#000000'
+theme.taglist_squares_sel = theme_assets.taglist_squares_sel(dpi(0), '#ff62d4')
+theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(dpi(0), '#ffffff')
+
+---- Prompt
+theme.prompt_bg = '#000000'
+theme.prompt_bg_cursor = '#ffffff'
+theme.prompt_fg = '#ffffff'
+theme.prompt_fg_cursor = '#000000'
+
+---- Hotkey
+theme.hotkeys_font = 'sans bold 10'
+theme.hotkeys_description_font = 'sans bold 10'
+theme.hotkeys_group_margin = dpi(20)
+theme.hotkeys_modifiers_fg = '#ff62d4'
+theme.hotkeys_label_bg = '#000000'
+theme.hotkeys_label_fg = '#ffffff'
+theme.hotkeys_bg = '#000000'
+theme.hotkeys_fg = '#ffffff'
+theme.hotkeys_border_color = '#ff62d4'
+theme.hotkeys_border_width = dpi(2)
+
+---- Notification
+theme.notification_font = 'sans bold 10'
+theme.notification_bg = '#000000'
+theme.notification_fg = '#ffffff'
+theme.notification_border_color = '#ff62d4'
+theme.notification_border_width = dpi(2)
+theme.notification_icon_size = dpi(32)
+theme.notification_width = dpi(360)
+theme.notification_height = dpi(60)
+theme.notification_margin = dpi(10)
+
+-- @Bug: awesome from arch official repo, theme.notification_xxxx not work
+-- @REF: https://www.reddit.com/r/awesomewm/comments/glyl6b/theming_weirdness/
+require('naughty').config.defaults.margin = theme.notification_margin
+require('naughty').config.defaults.border_width = theme.notification_border_width
+
+---- Awesome Icon
+theme.awesome_icon = theme_assets.awesome_icon(dpi(30), '#ff62d4', '#000000')
+
+---- Menu
+theme.menu_font = 'sans bold 10'
+theme.menu_bg_normal = '#000000'
+theme.menu_bg_focus = '#000000'
+theme.menu_fg_normal = '#ffffff'
+theme.menu_fg_focus = '#ff62d4'
+theme.menu_border_color = '#ff62d4'
+theme.menu_border_width = dpi(1)
+theme.menu_submenu_icon = themes_path..'default/submenu.png'
+theme.menu_height = dpi(30)
+theme.menu_width = dpi(180)
+
+---- Wallpaper
+theme.wallpaper = nil
+-- theme.wallpaper = themes_path..'default/background.png'
+
+---- TitleBar
+theme.titlebar_bg_focus = '#000000'
+theme.titlebar_bg_normal = '#000000'
+theme.titlebar_fg_focus = '#ffffff'
+theme.titlebar_fg_normal = '#1e1e1e'
+theme.titlebar_close_button_normal = themes_path..'default/titlebar/close_normal.png'
+theme.titlebar_close_button_focus = themes_path..'default/titlebar/close_focus.png'
+theme.titlebar_minimize_button_normal = themes_path..'default/titlebar/minimize_normal.png'
+theme.titlebar_minimize_button_focus = themes_path..'default/titlebar/minimize_focus.png'
+theme.titlebar_ontop_button_normal_inactive = themes_path..'default/titlebar/ontop_normal_inactive.png'
+theme.titlebar_ontop_button_focus_inactive = themes_path..'default/titlebar/ontop_focus_inactive.png'
+theme.titlebar_ontop_button_normal_active = themes_path..'default/titlebar/ontop_normal_active.png'
+theme.titlebar_ontop_button_focus_active = themes_path..'default/titlebar/ontop_focus_active.png'
+theme.titlebar_sticky_button_normal_inactive = themes_path..'default/titlebar/sticky_normal_inactive.png'
+theme.titlebar_sticky_button_focus_inactive = themes_path..'default/titlebar/sticky_focus_inactive.png'
+theme.titlebar_sticky_button_normal_active = themes_path..'default/titlebar/sticky_normal_active.png'
+theme.titlebar_sticky_button_focus_active = themes_path..'default/titlebar/sticky_focus_active.png'
+theme.titlebar_floating_button_normal_inactive = themes_path..'default/titlebar/floating_normal_inactive.png'
+theme.titlebar_floating_button_focus_inactive = themes_path..'default/titlebar/floating_focus_inactive.png'
+theme.titlebar_floating_button_normal_active = themes_path..'default/titlebar/floating_normal_active.png'
+theme.titlebar_floating_button_focus_active = themes_path..'default/titlebar/floating_focus_active.png'
+theme.titlebar_maximized_button_normal_inactive = themes_path..'default/titlebar/maximized_normal_inactive.png'
+theme.titlebar_maximized_button_focus_inactive = themes_path..'default/titlebar/maximized_focus_inactive.png'
+theme.titlebar_maximized_button_normal_active = themes_path..'default/titlebar/maximized_normal_active.png'
+theme.titlebar_maximized_button_focus_active = themes_path..'default/titlebar/maximized_focus_active.png'
+
+---- Layout
+theme.layout_fairh = themes_path..'default/layouts/fairhw.png'
+theme.layout_fairv = themes_path..'default/layouts/fairvw.png'
+theme.layout_floating = themes_path..'default/layouts/floatingw.png'
+theme.layout_magnifier = themes_path..'default/layouts/magnifierw.png'
+theme.layout_max = themes_path..'default/layouts/maxw.png'
+theme.layout_fullscreen = themes_path..'default/layouts/fullscreenw.png'
+theme.layout_tilebottom = themes_path..'default/layouts/tilebottomw.png'
+theme.layout_tileleft = themes_path..'default/layouts/tileleftw.png'
+theme.layout_tile = themes_path..'default/layouts/tilew.png'
+theme.layout_tiletop = themes_path..'default/layouts/tiletopw.png'
+theme.layout_spiral = themes_path..'default/layouts/spiralw.png'
+theme.layout_dwindle = themes_path..'default/layouts/dwindlew.png'
+theme.layout_cornernw = themes_path..'default/layouts/cornernww.png'
+theme.layout_cornerne = themes_path..'default/layouts/cornernew.png'
+theme.layout_cornersw = themes_path..'default/layouts/cornersww.png'
+theme.layout_cornerse = themes_path..'default/layouts/cornersew.png'
+
+---- Custom Icon Theme
+-- default path: /usr/share/icons, /usr/share/icons/hicolor
+theme.icon_theme = nil
+
+return theme
+-- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
---
+++ nvim/lua/plugin/init.lua
@@ -0,0 +1,179 @@
+return {
+  {
+    'catppuccin/nvim',
+    lazy = false,
+    config = require('plugin.config.catppuccin'),
+  },
+  {
+    'folke/noice.nvim',
+    lazy = true,
+    event = 'VeryLazy',
+    dependencies = {
+      { 'MunifTanjim/nui.nvim' },
+    },
+    config = require('plugin.config.noice'),
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    lazy = true,
+    event = 'InsertEnter',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function() require('lualine').setup() end,
+  },
+  {
+    'akinsho/bufferline.nvim',
+    lazy = true,
+    event = 'InsertEnter',
+    version = '*',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function() require('bufferline').setup() end,
+  },
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    lazy = true,
+    event = 'InsertEnter',
+    config = function() require('indent_blankline').setup() end,
+  },
+  {
+    'lewis6991/gitsigns.nvim',
+    lazy = true,
+    event = 'InsertEnter',
+    config = require('plugin.config.gitsigns'),
+  },
+  {
+    'rcarriga/nvim-notify',
+    lazy = true,
+    event = 'VeryLazy',
+    config = function() vim.notify = require('notify') end,
+  },
+  {
+    'nvim-tree/nvim-tree.lua',
+    lazy = true,
+    cmd = { 'NvimTreeToggle', 'NvimTreeOpen', 'NvimTreeFindFile', 'NvimTreeFindFileToggle', 'NvimTreeRefresh' },
+    config = require('plugin.config.nvim-tree'),
+  },
+  {
+    'akinsho/toggleterm.nvim',
+    lazy = true,
+    cmd = { 'ToggleTerm', 'ToggleTermSetName', 'ToggleTermToggleAll', 'ToggleTermSendVisualLines', 'ToggleTermSendCurrentLine', 'ToggleTermSendVisualSelection' },
+    config = require('plugin.config.toggleterm'),
+  },
+  {
+    'folke/trouble.nvim',
+    lazy = true,
+    cmd = { 'Trouble', 'TroubleToggle', 'TroubleRefresh' },
+    config = require('plugin.config.trouble'),
+  },
+  {
+    'folke/which-key.nvim',
+    lazy = true,
+    event = 'VeryLazy',
+    config = require('plugin.config.which-key'),
+  },
+  {
+    'nvim-telescope/telescope.nvim',
+    lazy = true,
+    cmd = 'Telescope',
+    dependencies = {
+      { 'nvim-tree/nvim-web-devicons' },
+      { 'nvim-lua/plenary.nvim' },
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+      { 'nvim-telescope/telescope-file-browser.nvim' },
+      { 'nvim-telescope/telescope-project.nvim' },
+      { 
+        'nvim-telescope/telescope-frecency.nvim', 
+        dependencies = {
+          { 'kkharji/sqlite.lua' },
+        } 
+      },
+      { 'nvim-telescope/telescope-live-grep-args.nvim' },
+    },
+    config = require('plugin.config.telescope'),
+  },
+  {
+    'phaazon/hop.nvim',
+    branch = 'v2',
+    cmd = { 'HopAnywhere', 'HopLine' },
+    config = function() require('hop').setup({ keys = 'etovxqpdygfblzhckisuran' }) end,
+  },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    lazy = true,
+    event = 'InsertEnter',
+    build = function()
+      if #vim.api.nvim_list_uis() ~= 0 then
+        vim.api.nvim_command('TSUpdate')
+      end
+    end,
+    dependencies = {
+      { 'nvim-treesitter/nvim-treesitter-textobjects' },
+      {
+        'nvim-treesitter/nvim-treesitter-context',
+        config = require('plugin.config.ts-context'),
+      },
+      {
+        'norcalli/nvim-colorizer.lua',
+        config = function() require('colorizer').setup() end,
+      },
+    },
+    config = require('plugin.config.treesitter'),
+  },
+  {
+    'hiphish/rainbow-delimiters.nvim',
+    lazy = true,
+    event = 'InsertEnter',
+    config = require('plugin.config.rainbow-delimiters'),
+  },
+  {
+    'numToStr/Comment.nvim',
+    lazy = true,
+    event = { 'CursorHold', 'CursorHoldI' },
+    config = function() require('Comment').setup() end,
+  },
+  {
+    'sindrets/diffview.nvim',
+    lazy = true,
+    cmd = { 'DiffviewOpen', 'DiffviewClose' },
+  },
+  {
+    'windwp/nvim-autopairs',
+    lazy = true,
+    event = 'InsertEnter',
+    config = function() require('nvim-autopairs').setup() end,
+  },
+  {
+    'hrsh7th/nvim-cmp',
+    lazy = true,
+    event = 'InsertEnter',
+    dependencies = {
+      {
+        'L3MON4D3/LuaSnip',
+        dependencies = { 'rafamadriz/friendly-snippets' },
+        config = require('plugin.config.luasnip'),
+      },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-nvim-lua' },
+      { 'hrsh7th/cmp-path' },
+      { 'hrsh7th/cmp-buffer' },
+    },
+    config = require('plugin.config.cmp'),
+  },
+  {
+    'neovim/nvim-lspconfig',
+    lazy = true,
+    event = 'InsertEnter',
+    dependencies = {
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
+    },
+    config = require('plugin.config.lsp'),
+  },
+  {
+    'nvimdev/lspsaga.nvim',
+    lazy = true,
+    event = 'LspAttach',
+    config = require('plugin.config.lspsaga'),
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+  },
+}
