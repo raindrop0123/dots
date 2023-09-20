80a76b8ce7c994e3a7bc681c37ad809370f0b9e5
raindrop0123 <139353546+raindrop0123@users.noreply.github.com>
2023-09-20 14:18:02 +0800

    Add files via upload

---
+++ nvim/lua/plugin/config/trouble.lua
@@ -0,0 +1,39 @@
+local config = {
+  position = 'bottom',
+  height = 15,
+  width = 50,
+  icons = true,
+  mode = 'document_diagnostics',
+  group = true,
+  padding = true,
+  action_keys = {
+    close = 'q',
+    cancel = '<Esc>',
+    refresh = 'r',
+    jump = { '<CR>', '<TAB>' },
+    open_split = { '<C-x>' },
+    open_vsplit = { '<C-v>' },
+    open_tab = { '<C-t>' },
+    jump_close = { 'o' },
+    toggle_mode = 'm',
+    toggle_preview = 'P',
+    hover = 'K',
+    preview = 'p',
+    close_folds = { 'zM', 'zm' },
+    open_folds = { 'zR', 'zr' },
+    toggle_fold = { 'zA', 'za' },
+    previous = 'k',
+    next = 'j',
+  },
+  indent_lines = true,
+  auto_open = false,
+  auto_close = false,
+  auto_preview = true,
+  auto_fold = false,
+  auto_jump = { 'lsp_definitions' },
+  use_diagnostic_signs = false,
+}
+
+return function()
+  require('trouble').setup(config)
+end
