7cababbf8f20ebcaca7f00d1b27bbd5069a611b2
raindrop0123 <139353546+raindrop0123@users.noreply.github.com>
2023-10-03 18:28:04 +0800

    Update indent-blankline.lua

--- nvim/lua/plugin/config/indent-blankline.lua
+++ nvim/lua/plugin/config/indent-blankline.lua
@@ -1,24 +1,3 @@
-local config = {
-  char = '│',
-  context_char = '┃',
-  show_first_indent_level = true,
-  filetype_exclude = {
-    '', -- for all buffers without a file type
-    'alpha',
-    'dashboard',
-    'fugitive',
-    'git',
-    'gitcommit',
-    'help',
-    'NvimTree',
-    'TelescopePrompt',
-    'undotree',
-  },
-  buftype_exclude = { 'terminal', 'nofile' },
-  show_current_context = true,
-  space_char_blankline = ' ',
-}
-
 return function()
   require('ibl').setup(config)
 end
