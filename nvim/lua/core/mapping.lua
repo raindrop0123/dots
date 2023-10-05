1d4f5ab9365a8dd037c2f8ef84ab38aa9ffcf987
raindrop0123 <139353546+raindrop0123@users.noreply.github.com>
2023-10-05 12:46:43 +0800

    Delete nvim directory

--- nvim/lua/core/mapping.lua
+++
@@ -1,7 +0,0 @@
-vim.keymap.set('i', 'jk', '<Esc>')
-vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Go to left window', remap = true })
-vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Go to lower window', remap = true })
-vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Go to upper window', remap = true })
-vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Go to right window', remap = true })
-vim.keymap.set('n', '<A-[>', '<Cmd>vertical resize -5<CR>', { desc = 'Window Resize -5', remap = true })
-vim.keymap.set('n', '<A-]>', '<Cmd>vertical resize +5<CR>', { desc = 'Window Resize +5', remap = true })
