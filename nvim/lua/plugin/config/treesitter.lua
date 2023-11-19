464344379a4159170b38ab0e71914c2806607f77
raindrop0123 <139353546+raindrop0123@users.noreply.github.com>
2023-11-19 19:53:10 +0800

    Add files via upload

---
+++ nvim/lua/plugin/config/treesitter.lua
@@ -0,0 +1,52 @@
+return function()
+  require('nvim-treesitter.configs').setup({
+    ensure_installed = {
+      'c',
+      'cpp',
+      'lua',
+      'markdown',
+      'markdown_inline',
+      'python',
+    },
+    highlight = {
+      enable = true,
+      disable = {},
+      additional_vim_regex_highlighting = { 'c', 'cpp' },
+    },
+    textobjects = {
+      select = {
+        enable = true,
+        keymaps = {
+          ['af'] = '@function.outer',
+          ['if'] = '@function.inner',
+          ['ac'] = '@class.outer',
+          ['ic'] = '@class.inner',
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        goto_next_start = {
+          [']['] = '@function.outer',
+          [']m'] = '@class.outer',
+        },
+        goto_next_end = {
+          [']]'] = '@function.outer',
+          [']M'] = '@class.outer',
+        },
+        goto_previous_start = {
+          ['[['] = '@function.outer',
+          ['[m'] = '@class.outer',
+        },
+        goto_previous_end = {
+          ['[]'] = '@function.outer',
+          ['[M'] = '@class.outer',
+        },
+      },
+    },
+    rainbow = {
+      enable = true,
+      extended_mode = true,
+    },
+  })
+end
---
+++ nvim/lua/plugin/init.lua
@@ -0,0 +1,168 @@
+return {
+  {
+    'catppuccin/nvim',
+    lazy = false,
+    -- event = 'InsertEnter',
+    -- priority = 1000,
+    config = require('plugin.config.catppuccin'),
+  },
+  {
+    'akinsho/bufferline.nvim',
+    lazy = true,
+    event = 'InsertEnter',
+    version = '*',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = true,
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    lazy = true,
+    event = 'InsertEnter',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = true,
+  },
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    lazy = true,
+    main = 'ibl',
+    event = 'InsertEnter',
+    config = true,
+  },
+  {
+    'rcarriga/nvim-notify',
+    lazy = true,
+    event = 'VeryLazy',
+    config = function() vim.notify = require('notify') end,
+  },
+  {
+    'nvim-tree/nvim-tree.lua',
+    lazy = true,
+    cmd = {
+      'NvimTreeToggle',
+      'NvimTreeOpen',
+      'NvimTreeFindFile',
+      'NvimTreeFindFileToggle',
+      'NvimTreeRefresh'
+    },
+    config = require('plugin.config.nvim-tree'),
+  },
+  {
+    'folke/trouble.nvim',
+    lazy = true,
+    cmd = { 'Trouble', 'TroubleToggle', 'TroubleRefresh' },
+    config = require('plugin.config.trouble'),
+  },
+  {
+    'akinsho/toggleterm.nvim',
+    lazy = true,
+    cmd = {
+      'ToggleTerm',
+      'ToggleTermSetName',
+      'ToggleTermToggleAll',
+      'ToggleTermSendVisualLines',
+      'ToggleTermSendCurrentLine',
+      'ToggleTermSendVisualSelection'
+    },
+    config = require('plugin.config.toggleterm'),
+  },
+  {
+    'folke/which-key.nvim',
+    lazy = true,
+    event = 'VeryLazy',
+    config = require('plugin.config.which-key'),
+  },
+  {
+    'nvim-telescope/telescope.nvim',
+    lazy = true,
+    cmd = 'Telescope',
+    dependencies = {
+      { 'nvim-lua/plenary.nvim' },
+      { 'nvim-tree/nvim-web-devicons' },
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+      { 'nvim-telescope/telescope-file-browser.nvim' },
+      { 'nvim-telescope/telescope-project.nvim' },
+      { 'nvim-telescope/telescope-frecency.nvim' },
+      { 'nvim-telescope/telescope-live-grep-args.nvim' },
+    },
+    config = require('plugin.config.telescope'),
+  },
+  {
+    'phaazon/hop.nvim',
+    branch = 'v2',
+    cmd = { 'HopAnywhere', 'HopLine' },
+    config = true,
+  },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    lazy = true,
+    event = 'InsertEnter',
+    build = function()
+      if #vim.api.nvim_list_uis() ~= 0 then
+        vim.api.nvim_command('TSUpdate')
+      end
+    end,
+    dependencies = {
+      { 'nvim-treesitter/nvim-treesitter-textobjects' },
+      { 'norcalli/nvim-colorizer.lua' },
+      { 'p00f/nvim-ts-rainbow' },
+      {
+        'nvim-treesitter/nvim-treesitter-context',
+        config = require('plugin.config.treesitter-context'),
+      },
+    },
+    config = require('plugin.config.treesitter'),
+  },
+  {
+    'numToStr/Comment.nvim',
+    lazy = true,
+    event = { 'CursorHold', 'CursorHoldI' },
+    config = true,
+  },
+  {
+    'sindrets/diffview.nvim',
+    lazy = true,
+    cmd = { 'DiffviewOpen', 'DiffviewClose' },
+  },
+  {
+    'windwp/nvim-autopairs',
+    lazy = true,
+    event = 'InsertEnter',
+    config = true,
+  },
+  {
+    'hrsh7th/nvim-cmp',
+    lazy = true,
+    event = { 'InsertEnter', 'CmdlineEnter' },
+    dependencies = {
+      {
+        'L3MON4D3/LuaSnip',
+        dependencies = { 'rafamadriz/friendly-snippets' },
+        config = require('plugin.config.luasnip'),
+      },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-nvim-lua' },
+      { 'hrsh7th/cmp-path' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-cmdline' },
+    },
+    config = require('plugin.config.cmp'),
+  },
+  {
+    'nvimdev/lspsaga.nvim',
+    lazy = true,
+    event = 'LspAttach',
+    config = require('plugin.config.lspsaga'),
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+  },
+  {
+    'neovim/nvim-lspconfig',
+    lazy = true,
+    event = 'InsertEnter',
+    dependencies = {
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
+    },
+    config = require('plugin.config.lsp'),
+  },
+}
