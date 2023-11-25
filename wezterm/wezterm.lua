064347593e8bf6cddcb2cb5a0cd116359adedaf7
raindrop0123 <139353546+raindrop0123@users.noreply.github.com>
2023-11-26 00:26:24 +0800

    Create wezterm.lua

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,23 @@
+local wezterm = require('wezterm')
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- Theme
+config.color_scheme = 'Catppuccin Mocha'
+
+-- Font
+config.font_size = 12.0
+config.font = wezterm.font('Hack Nerd Font', { italic = false })
+
+-- Do not show confirm message when closing window
+config.window_close_confirmation = 'AlwaysPrompt'
+
+-- and finally, return the configuration to wezterm
+return config
