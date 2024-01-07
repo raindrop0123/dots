cd2575e6bd23b0fe5309ad052025edbbcc088728
raindrop0123 <139353546+raindrop0123@users.noreply.github.com>
2024-01-07 21:19:43 +0800

    Add files via upload

---
+++ emacs/lisp/init-misc.el
@@ -0,0 +1,33 @@
+;;; init-misc.el --- -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(require-package 'which-key)
+(add-hook 'after-init-hook #'which-key-mode)
+
+(require-package 'diredfl)
+(add-hook 'dired-mode-hook #'diredfl-mode)
+
+(require-package 'avy)
+(global-set-key (kbd "M-g w") #'avy-goto-word-0)
+(global-set-key (kbd "M-g l") #'avy-goto-line)
+
+(require-package 'ace-window)
+(global-set-key [remap other-window] 'ace-window)
+(global-set-key (kbd "M-o") 'ace-window)
+(with-eval-after-load 'ace-window
+  (setq aw-background nil)
+  (setq aw-char-position 'top-left)
+  (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l)))
+
+(require-package 'gcmh)
+(add-hook 'after-init-hook #'gcmh-mode)
+
+(require-package 'xclip)
+(add-hook 'after-init-hook #'xclip-mode)
+
+(require-package 'esup)
+(require-package 'sudo-edit)
+
+(provide 'init-misc)
+;;; init-misc.el ends here
---
+++ emacs/lisp/init-prog.el
@@ -0,0 +1,21 @@
+;;; init-prog.el --- -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(require-package 'flycheck)
+(add-hook 'prog-mode-hook #'global-flycheck-mode)
+
+(require-package 'rainbow-delimiters)
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+(require-package 'symbol-overlay)
+(add-hook 'prog-mode-hook #'symbol-overlay-mode)
+
+(require-package 'lua-mode)
+(with-eval-after-load 'lua-mode
+  (setq lua-indent-level 2)
+  (setq lua-indent-nested-block-content-align nil)
+  (setq lua-indent-close-paren-align nil))
+
+(provide 'init-prog)
+;;; init-prog.el ends here
