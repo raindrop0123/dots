a30a2f8803367fd969d8bba8d804ca6d6019be5d
raindrop0123 <139353546+raindrop0123@users.noreply.github.com>
2024-01-18 22:59:49 +0800

    Delete nvim directory

--- nvim/init.lua
+++
@@ -1,9 +0,0 @@
-if not vim.g.vscode then
-  require('core.global')
-  require('core.clipboard')
-  require('core.autocmd')
-  require('core.option')
-  require('core.disable_distribution_plugin')
-  require('core.mapping')
-  require('core.lazy')
-end
