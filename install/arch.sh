198822fecf30d13029148ad08eb11db02866133c
raindrop0123 <139353546+raindrop0123@users.noreply.github.com>
2024-01-22 16:31:35 +0800

    Update arch.sh

--- install/arch.sh
+++ install/arch.sh
@@ -131,7 +131,7 @@ arch-chroot /mnt systemctl enable NetworkManager.service
 
 # Official Package
 PKG="xorg-xinit xorg-server awesome"
-PKG="$PKG xclip ibus ibus-chewing alacritty"
+PKG="$PKG xclip ibus ibus-chewing kitty"
 PKG="$PKG emacs ripgrep lsd"
 PKG="$PKG alsa-utils brightnessctl pcmanfm flameshot"
 arch-chroot /mnt sudo pacman -S --noconfirm --needed $PKG
