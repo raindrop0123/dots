1981d6cf733dc70bfa62faf1aa260125dafc87a3
raindrop0123 <139353546+raindrop0123@users.noreply.github.com>
2024-02-12 01:54:19 +0800

    Add files via upload

---
+++ emacs/init.el
@@ -0,0 +1,21 @@
+;;; init.el --- -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+(let* ((gc-cons-threshold most-positive-fixnum)
+       (gc-cons-percentage 0.6)
+       (file-name-handler-alist nil))
+  (require 'init-core)
+  (require 'init-company)
+  (require 'init-evil)
+  (require 'init-misc))
+
+(message "[Emacs] Loaded in %s with %d garbage collections with %d packages."
+         (format "%.4f seconds" (float-time (time-subtract after-init-time before-init-time)))
+         gcs-done
+         (length package-alist))
+
+(provide 'init)
+;;; init.el ends here
