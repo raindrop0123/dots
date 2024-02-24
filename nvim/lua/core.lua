7b397382b9fdbfd75cdc474e2bb6490662096745
raindrop0123 <139353546+raindrop0123@users.noreply.github.com>
2024-02-24 12:26:10 +0800

    Update core.lua

--- nvim/lua/core.lua
+++ nvim/lua/core.lua
@@ -249,8 +249,8 @@ require('lazy').setup({
   spec = {
     { 'LazyVim/LazyVim', import = 'lazyvim.plugins' },
     { import = 'lazyvim.plugins.extras.lsp.none-ls' },
-		{ import = 'lazyvim.plugins.extras.lang.clangd' },
-		{ import = 'lazyvim.plugins.extras.lang.python' },
+    { import = 'lazyvim.plugins.extras.lang.clangd' },
+    { import = 'lazyvim.plugins.extras.lang.python' },
     { import = 'lazyvim.plugins.extras.ui.mini-animate' },
     { import = 'plugins' },
   },
