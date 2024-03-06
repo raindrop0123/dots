8c2fbb8a98328255ce6a12c2c289628304d9e010
raindrop0123 <139353546+raindrop0123@users.noreply.github.com>
2024-03-06 22:13:23 +0800

    Add files via upload

---
+++ emacs/init.el
@@ -0,0 +1,23 @@
+;;; init.el --- -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+(let* ((gc-cons-threshold most-positive-fixnum)
+       (gc-cons-percentage 0.6)
+       (file-name-handler-alist nil))
+  (require 'init-base)
+  (require 'init-company)
+  (require 'init-evil)
+  (require 'init-minibuffer)
+  (require 'init-editor)
+  (require 'init-tool))
+
+(message "[Emacs] Loaded in %s with %d garbage collections with %d packages."
+         (format "%.4f seconds" (float-time (time-subtract after-init-time before-init-time)))
+         gcs-done
+         (length package-alist))
+
+(provide 'init)
+;;; init.el ends here
---
+++ emacs/lisp/init-editor.el
@@ -0,0 +1,18 @@
+;;; init-editor.el --- -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(require-package 'rainbow-mode)
+(add-hook 'prog-mode-hook #'rainbow-mode)
+
+(require-package 'rainbow-delimiters)
+(Add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+(require-package 'symbol-overlay)
+(run-with-idle-timer 2 nil #'symbol-overlay-mode)
+
+(require-package 'which-key)
+(run-with-idle-timer 2 nil #'which-key-mode)
+
+(provide 'init-editor)
+;;; init-editor.el ends here
