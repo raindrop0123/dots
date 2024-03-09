88b6ab295912992489103a1b3de7afb1a23a8873
raindrop0123 <139353546+raindrop0123@users.noreply.github.com>
2024-03-09 22:20:17 +0800

    Add files via upload

---
+++ emacs/lisp/init-base.el
@@ -0,0 +1,80 @@
+;;; init-base.el --- -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+;; C source code
+(setq-default scroll-step 1)
+(setq-default scroll-conservatively 10000)
+(setq-default auto-window-vscroll nil)
+(setq-default bidi-display-reordering nil)
+(add-hook 'minibuffer-setup-hook #'(lambda () (setq gc-cons-threshold most-positive-fixnum)))
+(add-hook 'minibuffer-exit-hook #'(lambda () (setq gc-cons-threshold 800000)))
+
+;; icomplete.el
+(add-hook 'after-init-hook #'icomplete-mode)
+
+;; package.el
+(package-initialize)
+(setq package-quickstart t)
+(setq package-check-signature nil)
+(setq package-enable-at-startup nil)
+(setq package-archives
+      '(;; ("nongnu" . "https://elpa.nongnu.org/nongnu/")
+        ;; ("elpa-devel" . "https://elpa.gnu.org/devel/")
+        ;; ("org" . "https://orgmode.org/elpa/")
+        ;; ("marmalade" . "http://marmalade-repo.org/packages/")
+        ;; ("melpa-stable" . "https://stable.melpa.org/packages/")
+        ("gnu" . "https://elpa.gnu.org/packages/")
+        ("melpa" . "https://melpa.org/packages/")))
+
+(defun require-package (package &optional min-version no-refresh)
+  (unless (featurep 'package)
+    (condition-case nil
+        (require 'package)
+      (error nil)))
+  (unless (package-installed-p package min-version)
+    (unless (or (assoc package package-archive-contents) no-refresh)
+      (message "Missing package: %s" package)
+      (package-refresh-contents))
+    (package-install package)))
+
+;; simple.el
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 2)
+(add-hook 'prog-mode-hook #'size-indication-mode)
+(add-hook 'prog-mode-hook #'line-number-mode)
+(add-hook 'prog-mode-hook #'column-number-mode)
+(add-hook 'prog-mode-hook #'auto-save-mode)
+
+;; so-long.el
+(add-hook 'prog-mode-hook #'global-so-long-mode)
+
+;; autorevert.el
+(add-hook 'prog-mode-hook #'global-auto-revert-mode)
+
+;; hl-line.el
+(add-hook 'prog-mode-hook #'global-hl-line-mode)
+
+;; display-line-numbers.el
+(add-hook 'prog-mode-hook #'display-line-numbers-mode)
+
+;; display-fill-column-indicator.el
+(add-hook 'prog-mode-hook #'display-fill-column-indicator-mode)
+
+;; paren.el
+(add-hook 'prog-mode-hook #'show-paren-mode)
+
+;; elec-pair
+(add-hook 'prog-mode-hook #'electric-pair-mode)
+
+;; loaddefs.el
+(add-hook 'prog-mode-hook #'save-place-mode)
+
+;; flymake.el
+(add-hook 'prog-mode-hook #'flymake-mode)
+
+;; ibuffer.el
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+(provide 'init-base)
+;;; init-base.el ends here
---
+++ emacs/lisp/init-pkg.el
@@ -0,0 +1,51 @@
+;;; init-pkg.el --- -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(require-package 'evil)
+(add-hook 'after-init-hook #'evil-mode)
+(with-eval-after-load 'evil
+  (setq evil-cross-lines t))
+
+(require-package 'evil-escape)
+(add-hook 'evil-mode-hook #'evil-escape-mode)
+(with-eval-after-load 'evil-escape
+  (setq evil-escape-key-sequence "jk")
+  (setq evil-escape-delay 0.2))
+
+(require-package 'evil-nerd-commenter)
+(with-eval-after-load 'evil-maps
+  (define-key evil-normal-state-map "gcc" #'evilnc-comment-or-uncomment-lines)
+  (define-key evil-visual-state-map "gc" #'evilnc-comment-or-uncomment-lines))
+
+(require-package 'company)
+(add-hook 'prog-mode-hook 'global-company-mode)
+(with-eval-after-load 'company
+  (setq company-idle-delay 0.1)
+  (setq company-minimum-prefix-length 1)
+  (setq company-show-numbers t)
+  (setq company-tooltip-align-annotations t)
+  (setq company-icon-margin 4)
+  (setq company-format-margin-function 'company-vscode-dark-icons-margin)
+  (setq company-dabbrev-ignore-case nil)
+  (setq company-dabbrev-downcase nil)
+  (setq company-backends '((company-capf :with company-yasnippet)
+                           (company-dabbrev-code company-keywords company-files)
+                           company-dabbrev))
+  (setq company-global-modes '(not
+                               eshell-mode
+                               shell-mode
+                               minibuffer-inactive-mode
+                               message-mode)))
+
+(require-package 'rainbow-delimiters)
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+(require-package 'which-key)
+(run-with-idle-timer 2 nil #'which-key-mode)
+
+(require-package 'xclip)
+(run-with-idle-timer 2 nil #'xclip-mode)
+
+(provide 'init-pkg)
+;;; init-pkg.el ends here
